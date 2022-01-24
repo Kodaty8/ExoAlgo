ac530a599e176e9e38915fc2ef279bb98098ca69
Julien Robin <julien.jean.robin@gmail.com>
2022-01-24 10:56:54 +0100

    Exercice 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	float prix = 10;
+    int[] reducs = {50,30};
+        System.out.println(reduc(prix, reducs));
     }
 
     public static float reduc(float prix, int[] reducs){
@@ -11,15 +13,16 @@ public class Main {
             System.out.println("He... Nan rien");
             return 0;
         }
+        prix = prix*(100-reducs[0])/100;
         for (int i=1; i<reducs.length; i++) {
             if (reducs[i]>reducs[i-1]){
                 System.out.println("Met tes remises en ordre");
                 return 0;
             }
-            prix = prix*reducs[i]/100;
+            prix = prix*(100-reducs[i])/100;
         }
-        int centimes = (int)prix*100;
-        prix = centimes/100;
+        int centimes = (int)(prix*100);
+        prix = (float)centimes/100;
         return prix;
     }
 }
